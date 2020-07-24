482febe05dfa99466154a429ee84cda5e3637ff7
Nathan Getachew <nathygech111@gmail.com>
2020-07-24 15:50:27 +0300

    fix: config file

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,46 +1,47 @@
 export const Config = {
-  global: {
-    app_version: '2.0.2',
-  },
-  env: {
-    development: {
-      public: {
-        EnvironmentConfig: {
-          RootAPIURL: 'http://35.184.137.237:8082/api/',
-        },
-      },
-      local: {
-        EnvironmentConfig: {
-          RootAPIURL: 'http://192.168.2.81:8084/api/',
-        },
-      },
-      jsiLocal: {
-        EnvironmentConfig: {
-          RootAPIURL: 'http://192.168.2.81:8085/api/',
-        },
-      },
+    "global": {
+        "app_version": "2.0.2"
     },
-    staging: {
-      public: {
-        EnvironmentConfig: {
-          RootAPIURL: 'http://35.184.137.237:8080/api/',
+    "env": {
+        "development": {
+            "public": {
+                "EnvironmentConfig": {
+                    "RootAPIURL": "http://35.184.137.237:8082/api/"
+                }
+            },
+            "local": {
+                "EnvironmentConfig": {
+                    "RootAPIURL": "http://192.168.2.81:8084/api/"
+                }
+            },
+            "jsiLocal": {
+                "EnvironmentConfig": {
+                    "RootAPIURL": "http://192.168.2.81:8085/api/"
+                }
+            }
         },
-      },
-      local: {
-        EnvironmentConfig: {
-          RootAPIURL: 'http://192.168.2.81:8084/api/',
+        "staging": {
+            "public": {
+                "EnvironmentConfig": {
+                    "RootAPIURL": "http://35.184.137.237:8080/api/"
+                }
+            },
+            "local": {
+                "EnvironmentConfig": {
+                    "RootAPIURL": "http://192.168.2.81:8084/api/"
+                }
+            },
+            "jsiLocal": {
+                "EnvironmentConfig": {
+                    "RootAPIURL": "http://192.168.2.81:8084/api/"
+                }
+            }
         },
-      },
-      jsiLocal: {
-        EnvironmentConfig: {
-          RootAPIURL: 'http://192.168.2.81:8084/api/',
-        },
-      },
-    },
-    production: {
-      EnvironmentConfig: {
-        RootAPIURL: '#{RootAPIURL}',
-      },
-    },
-  },
+        "production": {
+            "EnvironmentConfig": {
+                "RootAPIURL": "#{RootAPIURL}"
+            }
+        }
+
+    }
 };
