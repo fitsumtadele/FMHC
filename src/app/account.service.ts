d2d5962737050f2165fc4be3849e6ae8bfac99f0
Nathan Getachew <nathygech111@gmail.com>
2020-07-24 12:58:03 +0300

    login trial

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CommonService } from './common.service';
 import { StorageService } from './storage.service';
-import { AccountFactory } from './account.factory';
 import { AppConst } from './helper/constants';
 import * as _ from 'underscore';
 import { HttpClient } from '@angular/common/http'
@@ -13,60 +12,56 @@ import { HttpClient } from '@angular/common/http'
 })
 export class AccountService {
 
-  constructor(public commonservice: CommonService, public storageservice: StorageService, public accountfactory: AccountFactory, private http: HttpClient ) { }
+constructor(public commonservice: CommonService, public storageservice: StorageService, private http: HttpClient ) { }
    // var deferred = $q.defer();
-   /*login (loginModel) {
-    return this.http.post(this.commonservice.buildUrl((AppConst.API_URL.Account.Autenticate), loginModel)
-        .then(function(response) {
-            var data = response.data;
-            this.storageservice.set(AppConst.StorageKeys.Token, response.data.token);
-            this.storageservice.set(AppConst.StorageKeys.UserInfo, response.data.user);
-            this.storageservice.set(AppConst.StorageKeys.Permissions, response.data.permissions);
-            return data;
-        }, function(error) {
-            return error;
+    login (loginModel) {
+        return this.http.post(this.commonservice.buildUrl((AppConst.API_URL.Account.Autenticate), ''), loginModel);
+    }
+
+    loginSuccess(response) {
+        this.storageservice.set(AppConst.StorageKeys.Token, response.data.token);
+        this.storageservice.set(AppConst.StorageKeys.UserInfo, response.data.user);
+        this.storageservice.set(AppConst.StorageKeys.Permissions, response.data.permissions);
+        return response.data;
+    }
+
+    logout (reason) {
+        this.commonservice.buildUrl((AppConst.API_URL.Account.Logout), { userID: this.userInfo().id });
+        this.storageservice.removeAll();
+    }
+
+    userInfo () {
+        var userInfo = this.storageservice.get(AppConst.StorageKeys.UserInfo);
+        return userInfo;
+    };
+
+    token () {
+        var token = this.storageservice.get(AppConst.StorageKeys.Token);
+        if (token != undefined) {
+            return 'Bearer ' + token.access_token;
+        }
+        return null;
+    }
+
+    permissions () {
+        var permissions = this.storageservice.get(AppConst.StorageKeys.Permissions);
+        return permissions;
+    }
+
+    hasThisPermission (permission) {
+        var permissions = this.permissions();
+        var hasPermission = _.contains(permissions, permission);
+        return hasPermission;
+    }
+
+    hasThesePermissions (permissions) {
+        var hasPermission = false;
+        _.each(permissions, function(permission) {
+            //return true if user has access to one of the permissions
+            hasPermission = hasPermission || this.hasThisPermission(permission);
         })
+        return hasPermission;
     }
-    
-
-logout (reason) {
-     this.accountfactory.logout.save({ userID: null }, { userID: this.userInfo().id, reason:reason});
-
-    this.storageservice.removeAll();
-}*/
-
-userInfo () {
-    var userInfo = this.storageservice.get(AppConst.StorageKeys.UserInfo);
-    return userInfo;
-};
-
-token () {
-    var token = this.storageservice.get(AppConst.StorageKeys.Token);
-    if (token != undefined) {
-        return 'Bearer ' + token.access_token;
-    }
-    return null;
-}
-
-permissions () {
-    var permissions = this.storageservice.get(AppConst.StorageKeys.Permissions);
-    return permissions;
-}
-
-hasThisPermission (permission) {
-    var permissions = this.permissions();
-    var hasPermission = _.contains(permissions, permission);
-    return hasPermission;
-}
-
-hasThesePermissions (permissions) {
-    var hasPermission = false;
-    _.each(permissions, function(permission) {
-        //return true if user has access to one of the permissions
-        hasPermission = hasPermission || this.hasThisPermission(permission);
-    })
-    return hasPermission;
-}
 
 
 }
